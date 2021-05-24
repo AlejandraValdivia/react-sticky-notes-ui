404b0de6638c7ad810af93ab9084029a890ff701
Alejandra Valdivia <alexxandra_valdivia@hotmail.com>
2021-05-24 13:49:26 -0700

    Initial commit

---
+++ src/Note.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class Note extends Component {
+  updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = this.props.note.id;
+    this.props.onType(editMeId, "title", updatedValue);
+  };
+  updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = this.props.note.id;
+    this.props.onType(editMeId, "description", updatedValue);
+  };
+  render() {
+    return (
+      <li className="note">
+        <input
+          type="text"
+          value={this.props.note.title}
+          onChange={this.updateTitle}
+          placeholder="Title"
+          className="note__title"
+        />
+        <textarea
+          value={this.props.note.description}
+          onChange={this.updateDescription}
+          placeholder="Description..."
+          className="note__description"
+        />
+        <span className="note__delete">X</span>
+      </li>
+    );
+  }
+}
+
+export default Note;
